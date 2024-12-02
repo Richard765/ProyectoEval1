5cb070168997ccbb492c7dac567722c5340d263d
unknown <richardexposito04@gmail.com>
2024-12-02 18:38:37 +0100

    Hello world

---
+++ src/proyectoeval1/ProyectoEval1.java
@@ -0,0 +1,15 @@
+package proyectoeval1;
+
+/**
+ *
+ * @author Richard
+ */
+public class ProyectoEval1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
